21138f4d09b37d922cfd9cf0b921667ddd87c471
jack-snake123 <43805991+jack-snake123@users.noreply.github.com>
2022-09-05 21:28:51 +0900

    Add files via upload

---
+++ iko_materials_extraxt.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+cd /y/seclog/pyyiz001_tartest
+tar zxvf /tmp/ikou/config.yiscodef.tar.gz
+tar zxvf /tmp/ikou/hkn_inf.tar.gz
+tar zxvf /tmp/ikou/data.tar.gz
+tar zxvf /tmp/ikou/shell.tar.gz
+tar zxvf /tmp/ikou/yighhlog.tar.gz
+tar zxvf /tmp/ikou/yighilog.tar.gz
+tar zxvf /tmp/ikou/yighulog.tar.gz
+tar zxvf /tmp/ikou/yilhhlog.tar.gz
+tar zxvf /tmp/ikou/yilhilog.tar.gz
+tar zxvf /tmp/ikou/yilhulog.tar.gz
+tar zxvf /tmp/ikou/yilm.tar.gz
+tar zxvf /tmp/ikou/yilo.tar.gz
+tar zxvf /tmp/ikou/yilu.tar.gz
+tar zxvf /tmp/ikou/yilz.tar.gz
+tar zxvf /tmp/ikou/yioi.tar.gz
+tar zxvf /tmp/ikou/yiou.tar.gz
+tar zxvf /tmp/ikou/yiow.tar.gz
+tar zxvf /tmp/ikou/yipb.tar.gz
+tar zxvf /tmp/ikou/yisp.tar.gz
+tar zxvf /tmp/ikou/iko_tools.tar.gz
